b9edb49ea8e6210e662af8d7974166ea6adf21b2
theprivatepark <anneypark@gmail.com>
2020-10-05 20:42:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return array;
+}
+
+function countDown(num) {
+  for (let countdown = 10; countdown >= 0; countdown--) {
+    console.log(countdown);
+  }
+}
